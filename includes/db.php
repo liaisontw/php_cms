f45ece2211e2e4e6d2f2081ec1bfb3160a5cbd32
liaisontw <liaison.tw@gmail.com>
2021-11-16 10:39:32 +0800

    CMS section-91

--- includes/db.php
+++ includes/db.php
@@ -2,25 +2,24 @@
 
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
-$db['db_pass'] = "root";
+$db['db_pass'] = "#Wp.Local_0";
 $db['db_name'] = "cms";
 
 foreach($db as $key => $value){
-define(strtoupper($key), $value);
+    define(strtoupper($key), $value);
 }
 
 $connection = mysqli_connect(DB_HOST, DB_USER,DB_PASS,DB_NAME);
+//$query = "SET NAMES utf8";
+//mysqli_query($connection,$query);
 
 
-
-$query = "SET NAMES utf8";
-mysqli_query($connection,$query);
-
-//if($connection) {
-//
-//echo "We are connected";
-//
-//}
+//$connection = mysqli_connect("localhost", "root","#Wp.Local_0","cms");
+// if($connection) {
+//     echo "We are connected";
+// }else{
+//     echo "We are not connected";
+// }
 
 
 
