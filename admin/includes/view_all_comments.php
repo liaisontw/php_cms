7dc617bf5abe656f698a130ed131986420d95a15
liaisontw <liaison.tw@gmail.com>
2022-01-18 10:59:15 +0800

    CMS Section 131

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -35,23 +35,21 @@
         echo "<td>$comment_id</td>";
         echo "<td>$comment_author</td>";
         echo "<td>$comment_content</td>";
-
-        // $query = "SELECT * FROM categories WHERE cat_id = {$comment_category_id}";
-        // $select_categories_id = mysqli_query($connection, $query);
-
-        // while( $row = mysqli_fetch_assoc( $select_categories_id ) ) {
-        //     $cat_id = $row['cat_id'];
-        //     $cat_title = $row['cat_title'];
-        //     echo "<td>{$cat_title}</td>";
-        // }
-
         echo "<td>$comment_email</td>";
         echo "<td>$comment_status</td>";
-        echo "<td>Some Title</td>";
+        $query = " SELECT * FROM posts WHERE post_id = $comment_post_id ";
+        $select_post_id_query = mysqli_query($connection, $query);
+
+        while( $row = mysqli_fetch_assoc($select_post_id_query) ) {
+            $post_id = $row['post_id'];
+            $post_title = $row['post_title'];
+            echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
+        }
+        //echo "<td>Some Title</td>";
         echo "<td>$comment_date</td>";
         echo "<td><a href='posts.php?source=edit_post&p_id={$comment_id}'>Approve</a></td>";
         echo "<td><a href='posts.php?delete={$comment_id}'>Unapprove</a></td>";
-        echo "<td><a href='posts.php?delete={$comment_id}'>Delete</a></td>";
+        echo "<td><a href='comments.php?delete={$comment_id}'>Delete</a></td>";
         echo "</tr>";
     }
 
@@ -64,8 +62,10 @@
     if ( isset( $_GET[ 'delete' ] ) ) {
 
         $the_comment_id  = $_GET[ 'delete' ];
-        $query        = "DELETE FROM posts WHERE comment_id = {$the_comment_id}";
+        $query        = "DELETE FROM comments WHERE comment_id = {$the_comment_id}";
         $delete_query = mysqli_query( $connection, $query );
+        header("Location: comments.php");
+
     }
 
 ?>
