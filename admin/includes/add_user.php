deeb495ffff63c84ce50b90ec36f74b7cdf082ea
liaisontw <liaison.tw@gmail.com>
2022-02-21 12:43:43 +0800

    CMS Section 146

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,25 +1,24 @@
 <?php
-    if ( isset( $_POST['create_post'] ) ) {
-        $post_title           =  $_POST[ 'title' ];
-        $post_author          =  $_POST[ 'author' ];
-        $post_category_id     =  $_POST[ 'post_category' ];
-        $post_status          =  $_POST[ 'post_status' ];
-        $post_image           = $_FILES[ 'image' ][ 'name' ];
-        $post_image_temp      = $_FILES[ 'image' ][ 'tmp_name' ];
-        $post_tags            =  $_POST[ 'post_tags' ];
-        $post_content         =  $_POST[ 'post_content' ];
-        $post_date            =  date('d-m-y');
+    if ( isset( $_POST['create_user'] ) ) {
+        //$user_id        = $_POST['user_id'];
+        $user_firstname = $_POST['user_firstname'];
+        $user_lastname  = $_POST['user_lastname'];
+        $user_role      = $_POST['user_role'];
+        $username       = $_POST['username'];
+        $user_email     = $_POST['user_email'];
+        $user_password  = $_POST['user_password'];
+        
+        //$user_image     = $_POST['user_image'];
 
-
-        move_uploaded_file( $post_image_temp, "../images/$post_image" );
-        $query = "INSERT INTO posts(post_category_id, post_title,  
-                                    post_date,post_image,post_content,post_tags,post_status) ";
+        //move_uploaded_file( $post_image_temp, "../images/$post_image" );
+        $query = "INSERT INTO users(user_firstname, user_lastname,  
+                    user_role,username,user_email,user_password) ";
              
-        $query .= "VALUES({$post_category_id},'{$post_title}',
-                   now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') "; 
+        $query .= "VALUES('{$user_firstname}', '{$user_lastname}',  
+                    '{$user_role}','{$username}','{$user_email}','{$user_password}') "; 
 
-        $create_post_query = mysqli_query($connection, $query);  
-        confirmQuery( $create_post_query );
+        $create_user_query = mysqli_query($connection, $query);  
+        confirmQuery( $create_user_query );
         
     }
 
@@ -27,26 +26,20 @@
 
 <form action="" method="post" enctype="multipart/form-data">    
     <div class="form-group">
-        <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="title">
+        <label for="title">Firstname</label>
+        <input type="text" class="form-control" name="user_firstname">
     </div>
 
-    
     <div class="form-group">
-        <label for="category">Categories</label>
-        <select name="post_category" id="">
-<?php
-        $query = "SELECT * FROM categories";
-        $select_categories = mysqli_query($connection, $query);
-        confirmQuery( $select_categories );
+        <label for="title">Lastname</label>
+        <input type="text" class="form-control" name="user_lastname">
+    </div>
 
-        while( $row = mysqli_fetch_assoc($select_categories) ) {
-            $cat_id = $row['cat_id'];
-            $cat_title = $row['cat_title'];
-            echo "<option value='{$cat_id}'>{$cat_title}</option>";
-        }
-
-?>       
+    <div class="form-group">
+        <select name="user_role" id="">
+            <option value="subscriber">Select Options</option>
+            <option value="admin">Admin</option>
+            <option value="subscriber">Subscriber</option>
         </select>
     </div>
 
@@ -55,13 +48,7 @@
         <select name="post_user" id="">
 
         </select>
-    </div> -->
-
-    <div class="form-group">
-        <label for="title">Post Author</label>
-        <input type="text" class="form-control" name="author">
-    </div>
-
+    </div> 
     <div class="form-group">
         <select name="post_status" id="">
             <option value="draft">Post Status</option>
@@ -69,24 +56,31 @@
             <option value="draft">Draft</option>
         </select>
     </div>
-     
+
     <div class="form-group">
         <label for="post_image">Post Image</label>
         <input type="file"  name="image">
     </div>
 
+-->
+  
+
     <div class="form-group">
-        <label for="post_tags">Post Tags</label>
-        <input type="text" class="form-control" name="post_tags">
+        <label for="post_tags">Username</label>
+        <input type="text" class="form-control" name="username">
     </div>
      
     <div class="form-group">
-        <label for="post_content">Post Content</label>
-        <textarea class="form-control "name="post_content" id="" cols="30" rows="10">
-        </textarea>
+        <label for="post_content">Email</label>
+        <input type="email" class="form-control" name="user_email">
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <label for="post_content">Password</label>
+        <input type="password" class="form-control" name="user_password">
+    </div>
+
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="create_user" value="Add User">
     </div>
 </form>
--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -62,22 +62,22 @@
     </div>
 
     <div class="form-group">
-        <label for="category">Categories</label>
-        <select name="post_category" id="post_category">
+        <select name="user_role" id="">
 <?php
-        $query = "SELECT * FROM categories";
-        $select_categories = mysqli_query($connection, $query);
-        confirmQuery( $select_categories );
+        $query = "SELECT * FROM users";
+        $select_users = mysqli_query($connection, $query);
+        confirmQuery( $select_users );
 
-        while( $row = mysqli_fetch_assoc($select_categories) ) {
-            $cat_id = $row['cat_id'];
-            $cat_title = $row['cat_title'];
-            echo "<option value='{$cat_id}'>{$cat_title}</option>";
+        while( $row = mysqli_fetch_assoc($select_users) ) {
+            $user_id = $row['user_id'];
+            $user_role = $row['user_role'];
+            echo "<option value='{$user_id}'>{$user_role}</option>";
         }
 
 ?>       
         </select>
     </div>
+
 
     <!-- <div class="form-group">
         <label for="users">Users</label>
