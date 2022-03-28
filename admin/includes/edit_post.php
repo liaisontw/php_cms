368909a2207060036a059cd2056451670e7356f7
liaisontw <liaison.tw@gmail.com>
2022-03-28 11:08:31 +0800

    CMS Section 175

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -93,9 +93,14 @@
 
     <div class="form-group">
         <select name="post_status" id="">
-            <option value="draft">Post Status</option>
-            <option value="published">Published</option>
-            <option value="draft">Draft</option>
+            <option value='<?php echo $post_status; ?>''><?php echo $post_status; ?></option>
+            <?php
+                if ( $post_status == 'published' ) {
+                    echo "<option value='draft'>Draft</option>";
+                } else {
+                    echo "<option value='published'>Published</option>";
+                }
+            ?>
         </select>
     </div>
      
